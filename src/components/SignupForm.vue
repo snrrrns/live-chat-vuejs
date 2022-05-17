640be6e1ead3a08a7b9ffab0025b6a18c00bb710
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-05-17 18:56:07 +0900

    Add: ログイン画面、ユーザー登録画面を実装

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>アカウントを登録</h2>
+    <form @submit.prevent="signUp">
+      <input type="text" required placeholder="名前" v-model="name">
+      <input type="email" required placeholder="メールアドレス" v-model="email">
+      <input type="password" required placeholder="パスワード" v-model="password">
+      <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
+      <button>登録する</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  },
+  methods: {
+    async signUp() {
+      console.log(this.name, this.email, this.password, this.passwordConfirmation)
+    }
+  }
+}
+</script>
